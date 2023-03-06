3ba1a0094e08b632301ad4e762cc6bd6e9dc0d53
ogkunald <kunaldongare111@gmail.com>
2023-03-06 09:03:16 +0000

    Payment service MS initialized

---
+++ src/main/java/com/nastyzera/paymentservice/controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.nastyzera.paymentservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nastyzera.paymentservice.PaymentServiceApplication;
+import com.nastyzera.paymentservice.model.PaymentRequest;
+import com.nastyzera.paymentservice.model.PaymentResponse;
+import com.nastyzera.paymentservice.service.PaymentService;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest) {
+        return new ResponseEntity<>(
+                paymentService.doPayment(paymentRequest),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<PaymentResponse> getPaymentDetailsByOrderId(@PathVariable String orderId) {
+        return new ResponseEntity<>(
+                paymentService.getPaymentDetailsByOrderId(orderId),
+                HttpStatus.OK
+        );
+    }
+
+}
